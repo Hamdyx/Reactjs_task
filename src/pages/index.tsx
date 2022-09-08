15dab6d8ecd60cbf2cd67172acf7a8a6c574ae19
hamdyx <a.hamdy.nig@gmail.com>
2022-09-09 00:19:05 +0200

    refactor shop cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,24 @@ import { Breadcrumb, Layout, PageHeader } from 'antd';
 import { BreadcrumbSep } from '../icons/icons';
 
 const ShopPage: React.FC = () => {
+	const cards = [1, 2, 3, 4, 5];
+	const cardItems = cards.map((item) => <CardItem key={item} />);
 	return (
 		<Layout className="main-header">
 			<HeaderWithBreadcrumb />
+			<div className="shop-cards">{cardItems}</div>
 		</Layout>
+	);
+};
+const CardItem: React.FC<{ title?: string; details?: string }> = ({
+	title = 'CLOTHS',
+	details = '5 Items',
+}) => {
+	return (
+		<div className="card-item">
+			<h5>{title}</h5>
+			<p>{details}</p>
+		</div>
 	);
 };
 const HeaderWithBreadcrumb: React.FC = () => {
